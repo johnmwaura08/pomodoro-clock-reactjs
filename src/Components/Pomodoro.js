190bc4904994536f9944febdb80270b3cddeb49c
John Mwaura <johnmwaura08@gmail.com>
2020-08-28 00:01:46 -0500

    added buttons and onclick functions

--- src/Components/Pomodoro.js
+++ src/Components/Pomodoro.js
@@ -1,19 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Pomodoro extends Component {
-    state = {
-        breakCount: 5,
-        sessionCount: 25,
-        clockCount: 25 * 60,
-        currentTimer: "Session",
-        isPlaying: false
-      };
+  state = {
+    breakCount: 5,
+    sessionCount: 25,
+    clockCount: 25 * 60,
+    currentTimer: "Session",
+    isPlaying: false,
+  };
 
-    render() {
-        return (
-            <div>
-                
+  render() {
+    return (
+      <div>
+        <div>
+          <div className="flex">
+            <SetTimer {...breakProps} />
+            <SetTimer {...sessionProps} />
+          </div>
+
+          <div className="clock-container">
+            <h1 id="timer-label">{currentTimer}</h1>
+            <span id="time-left">{this.convertToTime(clockCount)}</span>
+
+            <div className="flex">
+              <button id="start_stop" onClick={this.handlePlayPause}>
+                <i className={`fas fa-${isPlaying ? "pause" : "play"}`} />
+              </button>
+              <button id="reset" onClick={this.handleReset}>
+                <i className="fas fa-sync" />
+              </button>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
