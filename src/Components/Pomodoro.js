cab41a4bc29a2f321bc128204a657ee2e438a8f2
John Mwaura <johnmwaura08@gmail.com>
2020-08-28 00:03:24 -0500

    added set timer function outside of the class component

--- src/Components/Pomodoro.js
+++ src/Components/Pomodoro.js
@@ -1,5 +1,27 @@
 import React, { Component } from "react";
 
+const SetTimer = (props) => {
+    const id = props.title.toLowerCase();
+  
+    return (
+      <div className="timer-container">
+        <h2 id={`${id}-label`}>{props.title} Length</h2>
+        <div className="flex actions-wrapper">
+          <button id={`${id}-decrement`} onClick={props.handleDecrease}>
+            <i className="fas fa-minus" />
+          </button>
+  
+          <span id={`${id}-length`}>{props.count}</span>
+  
+          <button id={`${id}-increment`} onClick={props.handleIncrease}>
+            <i className="fas fa-plus" />
+          </button>
+        </div>
+      </div>
+    );
+  };
+  
+
 export default class Pomodoro extends Component {
   state = {
     breakCount: 5,
