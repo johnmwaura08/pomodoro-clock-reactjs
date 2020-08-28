7f6e105b9156df98285d7467821d593aa9020cc7
John Mwaura <johnmwaura08@gmail.com>
2020-08-28 00:00:10 -0500

    added items to state

--- src/Components/Pomodoro.js
+++ src/Components/Pomodoro.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'
 
 export default class Pomodoro extends Component {
-    
+    state = {
+        breakCount: 5,
+        sessionCount: 25,
+        clockCount: 25 * 60,
+        currentTimer: "Session",
+        isPlaying: false
+      };
+
     render() {
         return (
             <div>
