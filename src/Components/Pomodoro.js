8fde8a48b92a61055d87c0b4b44deaa3fa7ffb0b
John Mwaura <johnmwaura08@gmail.com>
2020-08-28 00:09:22 -0500

    added audio tag, handlePlay and handleReset functions

--- src/Components/Pomodoro.js
+++ src/Components/Pomodoro.js
@@ -1,27 +1,26 @@
 import React, { Component } from "react";
 
 const SetTimer = (props) => {
-    const id = props.title.toLowerCase();
-  
-    return (
-      <div className="timer-container">
-        <h2 id={`${id}-label`}>{props.title} Length</h2>
-        <div className="flex actions-wrapper">
-          <button id={`${id}-decrement`} onClick={props.handleDecrease}>
-            <i className="fas fa-minus" />
-          </button>
-  
-          <span id={`${id}-length`}>{props.count}</span>
-  
-          <button id={`${id}-increment`} onClick={props.handleIncrease}>
-            <i className="fas fa-plus" />
-          </button>
-        </div>
-      </div>
-    );
-  };
-  
+  const id = props.title.toLowerCase();
 
+  return (
+    <div className="timer-container">
+      <h2 id={`${id}-label`}>{props.title} Length</h2>
+      <div className="flex actions-wrapper">
+        <button id={`${id}-decrement`} onClick={props.handleDecrease}>
+          <i className="fas fa-minus" />
+        </button>
+
+        <span id={`${id}-length`}>{props.count}</span>
+
+        <button id={`${id}-increment`} onClick={props.handleIncrease}>
+          <i className="fas fa-plus" />
+        </button>
+      </div>
+    </div>
+  );
+};
+const audio = document.getElementById("beep");
 export default class Pomodoro extends Component {
   state = {
     breakCount: 5,
@@ -38,33 +37,90 @@ export default class Pomodoro extends Component {
   componentWillUnmount() {
     clearInterval(this.loop);
   }
+  handlePlayPause = () => {
+    const { isPlaying } = this.state;
 
+    if (isPlaying) {
+      clearInterval(this.loop);
+
+      this.setState({
+        isPlaying: false,
+      });
+    } else {
+      this.setState({
+        isPlaying: true,
+      });
+
+      this.loop = setInterval(() => {
+        const {
+          clockCount,
+          currentTimer,
+          breakCount,
+          sessionCount,
+        } = this.state;
+
+        if (clockCount === 0) {
+          this.setState({
+            currentTimer: currentTimer === "Session" ? "Break" : "Session",
+            clockCount:
+              currentTimer === "Session" ? breakCount * 60 : sessionCount * 60,
+          });
+
+          audio.play();
+        } else {
+          this.setState({
+            clockCount: clockCount - 1,
+          });
+        }
+      }, 1000);
+    }
+  };
+
+  handleReset = () => {
+    this.setState({
+      breakCount: 5,
+      sessionCount: 25,
+      clockCount: 25 * 60,
+      currentTimer: "Session",
+      isPlaying: false,
+    });
+
+    clearInterval(this.loop);
+
+    audio.pause();
+    audio.currentTime = 0;
+  };
 
   render() {
     const {
-        breakCount,
-        sessionCount,
-        clockCount,
-        currentTimer,
-        isPlaying
-      } = this.state;
-  
-      const breakProps = {
-        title: "Break",
-        count: breakCount,
-        handleDecrease: () => this.handleLengthChange(-1, "break"),
-        handleIncrease: () => this.handleLengthChange(1, "break")
-      };
-  
-      const sessionProps = {
-        title: "Session",
-        count: sessionCount,
-        handleDecrease: () => this.handleLengthChange(-1, "session"),
-        handleIncrease: () => this.handleLengthChange(1, "session")
-      };
-  
+      breakCount,
+      sessionCount,
+      clockCount,
+      currentTimer,
+      isPlaying,
+    } = this.state;
+
+    const breakProps = {
+      title: "Break",
+      count: breakCount,
+      handleDecrease: () => this.handleLengthChange(-1, "break"),
+      handleIncrease: () => this.handleLengthChange(1, "break"),
+    };
+
+    const sessionProps = {
+      title: "Session",
+      count: sessionCount,
+      handleDecrease: () => this.handleLengthChange(-1, "session"),
+      handleIncrease: () => this.handleLengthChange(1, "session"),
+    };
+
     return (
       <div>
+        <audio
+          id="beep"
+          preload="auto"
+          src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+        />
         <div>
           <div className="flex">
             <SetTimer {...breakProps} />
